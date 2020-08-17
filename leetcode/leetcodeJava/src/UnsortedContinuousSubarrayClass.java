ac784c8b43e7bc75ee622f7bc34564081af8c802
jacen <412943134@qq.com>
2020-08-18 00:06:25 +0800

    581最短无序连续子数组

---
+++ leetcode/leetcodeJava/src/UnsortedContinuousSubarrayClass.java
@@ -0,0 +1,17 @@
+public class UnsortedContinuousSubarrayClass {
+    public int findUnsortedSubarray(int[] nums) {
+        int left = nums.length;;
+        int right = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++){
+                if (nums[j] < nums[i]){
+                    left = Math.min(left, i);
+                    right = Math.max(right, j);
+                }
+            }
+        }
+
+        return right - left < 0 ? 0 : right - left + 1;
+    }
+}
